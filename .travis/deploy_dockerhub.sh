cf61b9f61974a512308294498f4c464451acf6dc
EC2 Default User <ec2-user@ip-172-31-22-19.us-west-2.compute.internal>
2017-11-01 17:31:59 +0000

    update travis

---
+++ .travis/deploy_dockerhub.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+docker login -u $DOCKER_USER -p $DOCKER_PASS
+if [ "$TRAVIS_BRANCH" = "master" ]; then
+    TAG="latest"
+else
+    TAG="$TRAVIS_BRANCH"
+fi
+docker build -f Dockerfile -t evan89/cicd-buzz:$TAG .
+docker push evan89/cicd-buzz
+
